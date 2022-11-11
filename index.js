7e4ac458edcbb2e6f254c4ee5cb5f18fc398fedd
maniyarmohammedsaqlain <85744349+maniyarmohammedsaqlain@users.noreply.github.com>
2022-11-11 12:44:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const button = document.querySelector("button");
+const h2El = document.querySelector("h2");
+const bgEl = document.querySelector("section");
+// const hexColorEl = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+button.addEventListener("click", () => {
+let color = "#";
+color += Math.random().toString(16).slice(2, 8).toUpperCase();
+bgEl.style.backgroundColor = color;
+});
